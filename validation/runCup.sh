fe09233ec856989de26a0cefe666798605a79119
alandau <alandau@cs.stanford.edu>
2015-06-03 14:37:59 -0700

    Add JFlex, Cup libraries, placeholders for scripts to run them.

---
+++ validation/runCup.sh
@@ -0,0 +1,4 @@
+#Instructions at http://www2.cs.tum.edu/projects/cup/install.php
+#and http://www2.cs.tum.edu/projects/cup/docs.php#running
+
+java -jar lib/java-cup-11b.jar myparserspec.cup
---
+++ validation/runFlex.sh
@@ -0,0 +1,4 @@
+# Instructions for the command line at http://jflex.de/installing.html
+
+java -jar jflex-1.6.1.jar <options> <inputfiles>
+
