2f884172bc93ed69c4688567bd86c6ec20bb9bb0
Alex <alandau@cs.stanford.edu>
2015-12-23 21:58:32 -0800

    Delete the validation project.
    This has moved to its own repository:
    https://github.com/AlexLandau/gdl-validation

--- validation/runFlex.bat
+++
@@ -1,4 +0,0 @@
-REM Instructions for the command line at http://jflex.de/installing.html
-
-java -jar lib/jflex-1.6.1.jar -d src/main/java/net/alloyggp/griddle/generated/ schemas/GdlScanner.flex
-
--- validation/src/main/java/net/alloyggp/griddle/grammar/GdlVisitable.java
+++
@@ -1,5 +0,0 @@
-package net.alloyggp.griddle.grammar;
-
-public interface GdlVisitable {
-    void accept(GdlVisitor visitor);
-}
